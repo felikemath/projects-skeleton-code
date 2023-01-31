0236b35e6c5a5a393d446d3d24824bb414ccb94d
felikemath <michaelsong828@gmail.com>
2023-01-30 18:57:01 -0800

    Edited StartingDataset to now be able to load training data

--- data/StartingDataset.py
+++ data/StartingDataset.py
@@ -1,5 +1,8 @@
 import torch
+from torchvision import transforms
+from torchvision import datasets
 import pandas as pd
+from PIL import Image
 
 
 class StartingDataset(torch.utils.data.Dataset):
@@ -8,13 +11,20 @@ class StartingDataset(torch.utils.data.Dataset):
     """
 
     def __init__(self):
-        pass
+        self.data = pd.read_csv("train.csv")
+
+
 
     def __getitem__(self, index):
         inputs = torch.zeros([3, 224, 224])
-        label = 0
+        label = self.data.iloc[index][1]
+        convert_tensor = transforms.ToTensor()
+        img = Image.open("train\{0}".format(self.data.iloc[index][0]))
+        inputs = convert_tensor(img)
+        # img.show() to display image
+
 
         return inputs, label
 
     def __len__(self):
-        return 10000
+        return len(self.data)
